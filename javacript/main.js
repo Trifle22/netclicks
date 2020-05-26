cd39eade705a2db453afdb21062ba5a2fa294e22
Trifle22 <xromchenkov99@gmail.com>
2020-05-26 18:15:38 +0300

    second tuesday

--- javacript/main.js
+++ javacript/main.js
@@ -23,3 +23,16 @@ leftMenu.addEventListener('click', () => {
         hamburger.classList.add('open');
     }
 });
+// vard change on hover
+const card = document.querySelectorAll('.tv-card');
+let cardImg = document.querySelectorAll('.tv-card__img');
+cardImg.forEach(function(elem) {
+    let imgSrc = elem.getAttribute('src');
+    let imgData = elem.getAttribute('data-backdrop');
+    elem.addEventListener('mousemove', () => {
+        elem.setAttribute('src', imgData);
+    });
+    elem.addEventListener('mouseout', () => {
+        elem.setAttribute('src', imgSrc);
+    });
+});
