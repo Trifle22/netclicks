d32a79f6eb2a61b9640fe31ac27dbfb4d8b72463
Trifle22 <xromchenkov99@gmail.com>
2020-05-26 21:07:46 +0300

    second tue

--- javacript/main.js
+++ javacript/main.js
@@ -23,7 +23,7 @@ leftMenu.addEventListener('click', () => {
         hamburger.classList.add('open');
     }
 });
-// vard change on hover
+// card change on hover
 const card = document.querySelectorAll('.tv-card');
 let cardImg = document.querySelectorAll('.tv-card__img');
 cardImg.forEach(function(elem) {
@@ -36,3 +36,24 @@ cardImg.forEach(function(elem) {
         elem.setAttribute('src', imgSrc);
     });
 });
+// открытие модального окна
+tvShowsList = document.querySelector('.tv-shows__list');
+modal = document.querySelector('.modal');
+tvShowsList.addEventListener('click', event => {
+    const target = event.target;
+    const card = target.closest('.tv-card');
+    if (card) {
+        document.body.style.overflow = 'hidden';
+        modal.classList.remove('hide');
+    }
+});
+// закрытие
+modal.addEventListener('click', event => {
+    if (event.target.closest('.cross') ||
+        event.target.classList.contains('modal')) {
+        document.body.style.overflow = '';
+        modal.classList.add('hide');
+
+    }
+});
+class Human
