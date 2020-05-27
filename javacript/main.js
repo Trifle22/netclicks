a2095ac5ca5c007972c2bbd74fb42390e6f36560
Trifle22 <xromchenkov99@gmail.com>
2020-05-27 14:29:28 +0300

    third tue

--- javacript/main.js
+++ javacript/main.js
@@ -1,6 +1,58 @@
+const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
+API_KEY = 'aa8cc5268ed1b8f9383f996d3cb2c248';
 // menu
 const leftMenu = document.querySelector('.left-menu');
 const hamburger = document.querySelector('.hamburger');
+const tvShowsList = document.querySelector('.tv-shows__list')
+
+//cards
+
+const DBService = class {
+    getData = async(url) => {
+        const res = await fetch(url);
+        if (res.ok) {
+            return res.json();
+        } else {
+            throw new Error(`Не удалось получить данные по адресу ${url}`);
+
+        }
+
+    }
+    getTestData = () => {
+        return this.getData('test.json')
+    }
+}
+const renderCard = response => {
+    console.log(response);
+    tvShowsList.textContent = '';
+    response.results.forEach(item => {
+        const {
+            backdrop_path: backdrop,
+            name: title,
+            poster_path: poster,
+            vote_average: vote,
+        } = item;
+        const posterIMG = poster ? IMG_URL + poster : 'img/no-poster.jpg';
+        const backdropIMG = backdrop ? IMG_URL + backdrop : 'img/no-poster.jpg';
+        const voteElem = '';
+
+        const card = document.createElement('li');
+        card.className = 'tv-shows__item';
+        card.innerHTML = `
+        <a href="#" class="tv-card">
+              <span class="tv-card__vote">${vote}</span>
+              <img class="tv-card__img" 
+              src="${posterIMG}" 
+              data-backdrop="${backdropIMG}" 
+              alt="${title}">
+              <h4 class="tv-card__head">${title}</h4>
+        </a>
+        `;
+        tvShowsList.append(card);
+    })
+
+};
+new DBService().getTestData().then(renderCard);
 
 //открытие/закрытие меню
 hamburger.addEventListener('click', () => {
@@ -24,20 +76,18 @@ leftMenu.addEventListener('click', () => {
     }
 });
 // card change on hover
-const card = document.querySelectorAll('.tv-card');
-let cardImg = document.querySelectorAll('.tv-card__img');
-cardImg.forEach(function(elem) {
-    let imgSrc = elem.getAttribute('src');
-    let imgData = elem.getAttribute('data-backdrop');
-    elem.addEventListener('mousemove', () => {
-        elem.setAttribute('src', imgData);
-    });
-    elem.addEventListener('mouseout', () => {
-        elem.setAttribute('src', imgSrc);
-    });
-});
+const changeImage = event => {
+    const card = event.target.closest('.tv-shows__item');
+    if (card) {
+        const img = card.querySelector('.tv-card__img');
+        [img.src, img.dataset.backdrop] = [img.dataset.backdrop, img.src]
+    }
+};
+tvShowsList.addEventListener('mouseover', changeImage);
+tvShowsList.addEventListener('mouseout', changeImage);
+
 // открытие модального окна
-tvShowsList = document.querySelector('.tv-shows__list');
+
 modal = document.querySelector('.modal');
 tvShowsList.addEventListener('click', event => {
     const target = event.target;
@@ -56,4 +106,3 @@ modal.addEventListener('click', event => {
 
     }
 });
-class Human
