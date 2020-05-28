bd83ccc35c744a167a618ea505d1560fb5c1dc37
Trifle22 <xromchenkov99@gmail.com>
2020-05-28 04:01:28 +0300

    last tue

--- javacript/main.js
+++ javacript/main.js
@@ -1,9 +1,22 @@
 const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
+const SERVER = 'https://api.themoviedb.org/3';
 API_KEY = 'aa8cc5268ed1b8f9383f996d3cb2c248';
 // menu
 const leftMenu = document.querySelector('.left-menu');
 const hamburger = document.querySelector('.hamburger');
 const tvShowsList = document.querySelector('.tv-shows__list')
+tvShows = document.querySelector('.tv-shows');
+modal = document.querySelector('.modal');
+const loading = document.createElement('div');
+loading.className = 'loading';
+tvCardImg = document.querySelector('.tv-card__img');
+modalTitle = document.querySelector('.modal__title');
+genresList = document.querySelector('.genres-list');
+rating = document.querySelector('.rating');
+description = document.querySelector('.description');
+modalLink = document.querySelector('.modal__link');
+searchForm = document.querySelector('.search__form');
+searchFormInput = document.querySelector('.search__form-input');
 
 //cards
 
@@ -21,7 +34,14 @@ const DBService = class {
     getTestData = () => {
         return this.getData('test.json')
     }
+    getTestCard = () => {
+        return this.getData('card.json');
+    }
+    getSearchResult = query => this.getData(`${this.SERVER}/search/tv?api_key=${this.API_KEY}&language=ru-RU&query=${query}`);
+    getTvShow = id => this.getData(`${this.SERVER}/tv/${id}?api_key=${this.API_KEY}&language=ru-RU`);
+
 }
+
 const renderCard = response => {
     console.log(response);
     tvShowsList.textContent = '';
@@ -34,7 +54,7 @@ const renderCard = response => {
         } = item;
         const posterIMG = poster ? IMG_URL + poster : 'img/no-poster.jpg';
         const backdropIMG = poster ? IMG_URL + backdrop : 'img/no-poster.jpg';
-        const voteElem = vote ? `<span class='tv-card__vote'>${vote}</span>` : '';
+        const voteElem = vote ? ` < span class = 'tv-card__vote' > $ { vote } < /span>` : '';
 
         const card = document.createElement('li');
         card.className = 'tv-shows__item';
@@ -48,13 +68,22 @@ const renderCard = response => {
               <h4 class="tv-card__head">${title}</h4>
         </a>
         `;
+        loading.remove();
         tvShowsList.append(card);
     })
 
 };
-new DBService().getTestData().then(renderCard);
+searchForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const value = searchFormInput.value.trim();
+    if (value) {
+        tvShows.append(loading);
+        new DBService().getSearchResult(value).then(renderCard);
+    }
+    searchFormInput.value = '';
+});
 
-//открытие/закрытие меню
+//окрытие/закрытие меню
 hamburger.addEventListener('click', () => {
     leftMenu.classList.toggle('openMenu');
     hamburger.classList.toggle('open');
@@ -89,11 +118,29 @@ tvShowsList.addEventListener('mouseout', changeImage);
 // открытие модального окна
 modal = document.querySelector('.modal');
 tvShowsList.addEventListener('click', event => {
+    event.preventDefault();
     const target = event.target;
     const card = target.closest('.tv-card');
     if (card) {
-        document.body.style.overflow = 'hidden';
-        modal.classList.remove('hide');
+        new DBService().getTestCard()
+            .then(data => {
+                console.log(data);
+                tvCardImg.src = IMG_URL + data.poster_path;
+                modalTitle.textContent = data.name;
+                /* genresList.innerHTML = data.genres.reduce((acc, item) => `${acc}<li>${item.name}</li>`, ''); */
+                genresList.textContent = '';
+                for (const item of data.genres) {
+                    genresList.innerHTML += `<li>${item.name}</li>`;
+                }
+                rating
+                description
+                modalLink
+
+            })
+            .then(() => {
+                document.body.style.overflow = 'hidden';
+                modal.classList.remove('hide');
+            })
     }
 });
 // закрытие
